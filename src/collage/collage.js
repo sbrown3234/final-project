bf2947ae745bfa612ce7db3bceaa8fe36b25ab5b
Carmen_IV <Carmen_IV@Carmen-IVs-MBP.home>
2018-01-08 13:50:40 -0500

    added styling to collage

--- src/collage/collage.js
+++ src/collage/collage.js
@@ -65,7 +65,6 @@ export default class Collage extends React.Component {
     const { canvas } = this.state
     var obj = canvas.getActiveObject()
     canvas.remove(obj)
-    console.log('canvas: ', canvas, obj)
   }
 
   handlePaste() {
@@ -82,12 +81,13 @@ export default class Collage extends React.Component {
 
     axios.post('/uploadCanvasImage', imgData).then((results) => {
       if(results.data.success) {
+        console.log('image uploaded: ', results.data.image)
         fabric.Image.fromURL(results.data.image, (oImg) => {
           canvas.add(oImg)
         })
       }
     }).catch((err) => {
-      console.log('err in collage:', err)
+      console.log('err in collage upload:', err)
     })
 
   }
@@ -108,7 +108,7 @@ export default class Collage extends React.Component {
 
       }
     }).catch((err) => {
-      console.log('err in collage:', err)
+      console.log('err in collage save:', err)
     })
   }
 
@@ -117,9 +117,8 @@ export default class Collage extends React.Component {
     const { canvas } = this.state
 
     var data = canvas.toDataURL()
-    console.log('data: ', data)
 
-    let transfer = {
+    var transfer = {
       data: data
     }
 
@@ -128,110 +127,127 @@ export default class Collage extends React.Component {
         console.log('success')
       }
     }).catch((err) => {
-        console.log('err in collage:', err)
-      })
+      console.log('err in collage submit:', err)
+    })
+  }
+
+  applyImageFilter(index, filterName) {
+    const { canvas } = this.state;
+    var obj = canvas.getActiveObject();
+    obj.filters[index] = filterName;
+    obj.applyFilters();
+    canvas.renderAll();
+  }
+
+  handleFilter(e) {
+    let f = fabric.Image.filters;
+    const { filters } = this.state
+    const index = filters.indexOf(e.target.id);
+
+    if (index == 0) {
+      this.applyImageFilter(index, this.checked && new f.Invert())
+    } else if (index == 1) {
+      this.applyImageFilter(index, this.checked && new f.Sepia())
+    } else if (index == 2) {
+      console.log('in handleFilter')
+      this.applyImageFilter(index, e.target.id && new f.Brightness({
+        brightness: parseFloat($('#brightness-value').value)
+      }));
+    }
+  }
+
+  applyFilterValue(index, prop, value) {
+    const { canvas } = this.state
+    var obj = canvas.getActiveObject();
+    obj.filters[index][prop] = value;
+    obj.applyFilters();
+    canvas.renderAll();
+  }
+
+  filterValue(e) {
+    const { filters } = this.state
+    const index = filters.indexOf(e.target.name);
+    if (index == 2) {
+      console.log('index: ', e.target.value)
+      this.applyFilterValue(2, e.target.name, e.target.value);
+    }
+  }
+
+
+  render() {
+
+    const { showChat, clipboard } = this.state
+    const { chatMessages, handleChange, sendMessage } = this.props
+
+    const styleCanv1 = {
+      border: '1px solid rgb(204, 204, 204)',
+      position: 'absolute',
+      width: '500px',
+      height: '500px',
+      left: '15px',
+      touchAction: 'none',
+      cursor: 'default'
     }
 
-    applyImageFilter(index, filterName) {
-      const { canvas } = this.state;
-      var obj = canvas.getActiveObject();
-      obj.filters[index] = filterName;
-      obj.applyFilters();
-      canvas.renderAll();
+    if (!this.props) {
+      return <div>Loading....</div>
     }
 
-    handleFilter(e) {
-      let f = fabric.Image.filters;
-      const { filters } = this.state
-      const index = filters.indexOf(e.target.id);
-
-      if (index == 0) {
-        this.applyImageFilter(index, e.target.id && new f.Invert())
-      } else if (index == 1) {
-        this.applyImageFilter(index, e.target.id && new f.Sepia())
-      } else if (index == 2) {
-        this.applyImageFilter(index, e.target.id && new f.Brightness({
-          brightness: parseFloat($('#brightness-value').value)
-        }));
-      }
-    }
-
-    applyFilterValue(index, prop, value) {
-      const { canvas } = this.state
-      var obj = canvas.getActiveObject();
-      obj.filters[index][prop] = value;
-      obj.applyFilters();
-      canvas.renderAll();
-    }
-
-    filterValue(e) {
-      const { filters } = this.state
-      const index = filters.indexOf(e.target.name);
-      if (index == 2) {
-        this.applyFilterValue(2, e.target.name, e.target.value);
-      }
-    }
-
-
-    render() {
-
-      const { showChat, clipboard } = this.state
-      const { chatMessages, handleChange, sendMessage } = this.props
-
-      const styleCanv1 = {
-        border: '1px solid rgb(204, 204, 204)',
-        position: 'absolute',
-        width: '500px',
-        height: '500px',
-        left: '15px',
-        touchAction: 'none',
-        cursor: 'default'
-      }
-
-      return (
-        <div id="can">
+    return (
+      <div id="canvas">
         <div id="collage-container">
-          <div id="canvas-container">
-          <canvas width="800" height="600" ref="canvasEl" style={styleCanv1} className="lower-canvas"></canvas>
-          <div id="save">
-            <button className="save-button" type="submit" onClick={(e)=> this.saveCanvas(e)}>Save Image!</button>
-            <button className="save-button" type="submit" onClick={(e)=> this.submitCanvas(e)}>Submit Image!</button>
-          </div>
-        </div>
-          <div className="controls">
-            <br/>
-            <h3>Handle Images:</h3>
-            <button onClick={this.handleCopy}>Copy Selected Object</button>
-            <button onClick={() => this.handlePaste()}>Paste Selected Object</button>
-            <button onClick={this.removeItem}>Remove Selected Object</button>
-            <h3>Filters: </h3>
-            <p>
-              <span>Invert: </span>
-              <input onChange={(e)=> this.handleFilter(e)} id="invert" type="checkbox" />
-            </p>
-            <p>
-              <span>Sepia: </span>
-              <input onChange={(e)=> this.handleFilter(e)} id="sepia" type="checkbox" />
-            </p>
 
-            <p>
-              <span>Brightness:</span>
-              <input onChange={(e)=> this.handleFilter(e)} id="brightness" type="checkbox" />
-              <label>
-                Value:
-                <input onChange={(e)=> this.filterValue(e)} id="brightness-value" name="brightness" value="1" min="0.2" max="2.2" step="0.003921" type="range" />
-              </label>
-            </p>
+
+          <div id="canvas-container">
+
+            <canvas width="800" height="600" ref="canvasEl" style={styleCanv1} className="lower-canvas"></canvas>
+            <div id="save">
+              <button className="save-button" type="submit" onClick={(e)=> this.saveCanvas(e)}>Save Image!</button>
+              <button className="save-button" type="submit" onClick={(e)=> this.submitCanvas(e)}>Submit Image!</button>
+            </div>
+
+          </div>
+
+          <div id="panel-container">
+
+            <div className="controls">
+              <br/>
+              <h3>Copy/Paste Images:</h3>
+              <button onClick={this.handleCopy}>Copy Selected Object</button>
+              <button onClick={() => this.handlePaste()}>Paste Selected Object</button>
+              <button onClick={this.removeItem}>Remove Selected Object</button>
+              <h3>Filters: </h3>
+              <p>
+                <span>Invert: </span>
+                <input onChange={(e)=> this.handleFilter(e)} id="invert" type="checkbox" />
+              </p>
+              <p>
+                <span>Sepia: </span>
+                <input onChange={(e)=> this.handleFilter(e)} id="sepia" type="checkbox" />
+              </p>
+
+              <p>
+                  <span>Brightness:</span>
+                  <input onClick={(e)=> this.handleFilter(e)} id="brightness" type="checkbox" />
+                <label> <br/>
+                  Value:
+                  <input onChange={(e)=> this.filterValue(e)} id="brightness-value" name="brightness" value="1" min="0.2" max="2.2" step="0.003921" type="range" />
+                </label>
+              </p>
 
 
             <input onChange={(e) => this.handleImg(e)} type="file" name="file" />
-            <div id="public-chat">
-              <h1 onClick={()=> this.toggleChat()}>Need Help?</h1>
-              {!!showChat && <Chat chatMessages={chatMessages} handleChange={handleChange} sendMessage={sendMessage} />}
-            </div>
           </div>
+          <div id="public-chat">
+            <h1 onClick={()=> this.toggleChat()}>Need Help?</h1>
+            {!!showChat && <Chat chatMessages={chatMessages} handleChange={handleChange} sendMessage={sendMessage} />}
+          </div>
+
         </div>
+
+
       </div>
-      )
-    }
-  }
+    </div>
+  )
+}
+}
